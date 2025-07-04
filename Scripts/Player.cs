339e5e55399463e24fe2d6d4ff6f10373982bdfe
mooolalina <kataprocenko55@gmail.com>
2025-07-04 18:39:54 +0400

    Jump mechanics

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -5,7 +5,19 @@ public class Player : MonoBehaviour
 {
     private Vector2 moveInput;
 
-    [SerializeField] private float speed = 5f; // Скорость движения
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f; // Чем больше, тем выше прыжок
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void OnEnable()
     {
@@ -14,6 +26,7 @@ public class Player : MonoBehaviour
         {
             playerInput.actions["Move"].performed += OnMove;
             playerInput.actions["Move"].canceled += OnMove;
+            playerInput.actions["Jump"].performed += OnJump;
         }
     }
 
@@ -24,17 +37,33 @@ public class Player : MonoBehaviour
         {
             playerInput.actions["Move"].performed -= OnMove;
             playerInput.actions["Move"].canceled -= OnMove;
+            playerInput.actions["Jump"].performed -= OnJump;
         }
-    }
-
-    private void OnMove(InputAction.CallbackContext context)
-    {
-        moveInput = context.ReadValue<Vector2>();
     }
 
     private void Update()
     {
-    Vector3 movement = new Vector3(moveInput.x, moveInput.y, 0) * speed * Time.deltaTime;
-    transform.Translate(movement);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput.x * speed, rb.linearVelocity.y);
+
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnMove called");
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        Debug.Log("Jump!");
+        if (isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
