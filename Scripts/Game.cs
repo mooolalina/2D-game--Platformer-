8b61bc7df6dccc9638155ac5473f864039de37a6
mooolalina <kataprocenko55@gmail.com>
2025-07-06 17:43:51 +0400

    Make the transition between scenes

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -5,11 +5,13 @@ public class Game : MonoBehaviour
 {
     public void RestartCurrentLevel()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void LoadNextLevel()
     {
+        Time.timeScale = 1f;
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         int nextSceneIndex = currentSceneIndex + 1;
 
