162e33bb4041f6a0e46b26692daf9db298c14f0d
mooolalina <kataprocenko55@gmail.com>
2025-07-06 15:27:04 +0400

    Animation for enemy's death

--- Scripts/MushroomEnemy.cs
+++ Scripts/MushroomEnemy.cs
@@ -6,10 +6,23 @@ public class MushroomEnemy : MonoBehaviour
     [SerializeField] private int health = 3;
     private float lastDamageTime;
 
+    private Animator animator;
+    private bool isDead = false;
+    private Collider2D col;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        col = GetComponent<Collider2D>();
+    }
+
     public void TakeDamage(int amount)
     {
+        if (isDead) return; // Не наносим урон, если уже мёртв
+
         health -= amount;
         Debug.Log("Грибок получил урон: " + amount + ", осталось здоровья: " + health);
+
         if (health <= 0)
         {
             Die();
@@ -18,12 +31,22 @@ public class MushroomEnemy : MonoBehaviour
 
     private void Die()
     {
+        if (isDead) return;
+
+        isDead = true;
         Debug.Log("Грибок умер");
-        Destroy(gameObject);
+
+        animator.SetTrigger("isDead");
+
+        if (col != null)
+            col.enabled = false; 
+        Destroy(gameObject, 1f);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (isDead) return;
+
         TryDamagePlayer(collision.gameObject);
     }
 
